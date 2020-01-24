e7ae5497666a47f0e59ad993b892f5dc4a116ba8
alperensahin <Jarrive33>
2020-01-24 18:08:03 +0300

    init

--- src/pgdp/space/Spaceuin.java
+++ src/pgdp/space/Spaceuin.java
@@ -7,6 +7,7 @@ public class Spaceuin extends Thread {
     Beacon destination;
     FlightRecorder flightRecorder;
     Beacon current;
+    int selectIndex = 0;
     public Spaceuin(Beacon start, Beacon destination, FlightRecorder flightRecorder) {
         // TODO
         this.start = start;
@@ -17,29 +18,39 @@ public class Spaceuin extends Thread {
         while (!done){
             BeaconConnection bc = getNextWay();
             if(bc.beacon().equals(current)){
+                selectIndex++;
+                if(selectIndex == current.connections().size()){
+                    //there is no way
+                }
                 continue;
             }
             flightRecorder.recordDeparture(current);
             current = bc.beacon();
-            flightRecorder.recordArrival(current);
+            if(bc.type() == ConnectionType.WORMHOLE){
+                FlightRecorder flightRecorderCopy = this.flightRecorder.createCopy();
+                Spaceuin nt = new Spaceuin(current,this.destination,flightRecorderCopy);
+                nt.start();
+                this.interrupt();
+            }
+            if(bc.type() == ConnectionType.NORMAL){
+                selectIndex = 0;
+                flightRecorder.recordArrival(current);
+            }
             if(current.equals(destination)){
                 flightRecorder.tellStory();
                 done = true;
             }
-//            if(bc.type() == ConnectionType.NORMAL){
-//
-//            }
-//            if(bc.type() == ConnectionType.WORMHOLE){
-//
-//            }
+
 
         }
     }
     public BeaconConnection getNextWay(){
-        int limit = current.connections().size();
-        int random  = (int) (Math.random() * 100);
-        random = random%limit;
-        return current.connections().get(random);
+        for (BeaconConnection bc : current.connections()){
+            if(bc.beacon().equals(destination)){
+                return bc;
+            }
+        }
+        return current.connections().get(selectIndex);
     }
     // TODO
 
